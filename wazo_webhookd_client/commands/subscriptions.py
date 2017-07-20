093f4784cdaa1a3fcc7b7a22f6e3a41367a459d6
Sebastien Duthil <sduthil@wazo.community>
2017-07-20 10:45:47 -0400

    subscriptions: improve HTTP headers

--- wazo_webhookd_client/commands/subscriptions.py
+++ wazo_webhookd_client/commands/subscriptions.py
@@ -8,29 +8,30 @@ from wazo_webhookd_client.command import WebhookdCommand
 class SubscriptionsCommand(WebhookdCommand):
 
     resource = 'subscriptions'
-    _headers = {'Accept': 'application/json'}
+    _ro_headers = {'Accept': 'application/json'}
+    _rw_headers = {'Accept': 'application/json', 'Content-Type': 'application/json'}
 
     def create(self, subscription):
-        r = self.session.post(self.base_url, json=subscription, headers=self._headers)
+        r = self.session.post(self.base_url, json=subscription, headers=self._rw_headers)
         self.raise_from_response(r)
         return r.json()
 
     def list(self):
-        r = self.session.get(self.base_url, headers=self._headers)
+        r = self.session.get(self.base_url, headers=self._ro_headers)
         self.raise_from_response(r)
         return r.json()
 
     def get(self, subscription_uuid):
-        r = self.session.get('{base}/{id}'.format(base=self.base_url, id=subscription_uuid), headers=self._headers)
+        r = self.session.get('{base}/{id}'.format(base=self.base_url, id=subscription_uuid), headers=self._ro_headers)
         self.raise_from_response(r)
         return r.json()
 
     def edit(self, subscription_uuid, subscription):
-        r = self.session.put('{base}/{id}'.format(base=self.base_url, id=subscription_uuid), json=subscription, headers=self._headers)
+        r = self.session.put('{base}/{id}'.format(base=self.base_url, id=subscription_uuid), json=subscription, headers=self._rw_headers)
         self.raise_from_response(r)
         return r.json()
 
     def delete(self, subscription_uuid):
-        r = self.session.delete('{base}/{id}'.format(base=self.base_url, id=subscription_uuid), headers=self._headers)
+        r = self.session.delete('{base}/{id}'.format(base=self.base_url, id=subscription_uuid), headers=self._ro_headers)
         self.raise_from_response(r)
 
