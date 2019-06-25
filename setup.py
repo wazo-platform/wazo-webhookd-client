1b3a66bc13a0e2b90bc886cf8c006cf3419dd98b
Mehdi Abaakouk <mabaakouk@wazo.io>
2019-06-25 14:54:54 +0200

    blackify the code

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-# Copyright 2017 The Wazo Authors  (see AUTHORS file)
+# Copyright 2017-2019 The Wazo Authors  (see the AUTHORS file)
 # SPDX-License-Identifier: GPL-3.0-or-later
 
 
@@ -10,21 +10,16 @@ from setuptools import find_packages
 setup(
     name='wazo_webhookd_client',
     version='1.0',
-
     description='a simple client library for the wazo webhookd HTTP interface',
-
     author='Wazo Authors',
     author_email='dev@wazo.community',
-
     url='http://wazo.community',
-
     packages=find_packages(),
-
     entry_points={
         'wazo_webhookd_client.commands': [
             'config = wazo_webhookd_client.commands.config:ConfigCommand',
             'status = wazo_webhookd_client.commands.status:StatusCommand',
             'subscriptions = wazo_webhookd_client.commands.subscriptions:SubscriptionsCommand',
-        ],
-    }
+        ]
+    },
 )
--- wazo_webhookd_client/command.py
+++ wazo_webhookd_client/command.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-# Copyright 2017 The Wazo Authors  (see AUTHORS file)
+# Copyright 2017-2019 The Wazo Authors  (see the AUTHORS file)
 # SPDX-License-Identifier: GPL-3.0-or-later
 
 from xivo_lib_rest_client.command import RESTCommand
@@ -10,7 +10,6 @@ from .exceptions import InvalidWebhookdError
 
 
 class WebhookdCommand(RESTCommand):
-
     @staticmethod
     def raise_from_response(response):
         if response.status_code == 503:
