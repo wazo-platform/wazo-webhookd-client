02c75ce606f7bed500ddc27a77bf805a083dacb4
Sebastien Duthil <sduthil@wazo.community>
2021-01-26 15:56:42 -0500

    errors: raise correct error when body is empty

--- wazo_webhookd_client/commands/config.py
+++ wazo_webhookd_client/commands/config.py
@@ -1,12 +1,12 @@
 # -*- coding: utf-8 -*-
-# Copyright 2017-2019 The Wazo Authors  (see AUTHORS file)
+# Copyright 2017-2021 The Wazo Authors  (see the AUTHORS file)
 # SPDX-License-Identifier: GPL-3.0-or-later
 
 
-from wazo_lib_rest_client import RESTCommand
+from wazo_webhookd_client.command import WebhookdCommand
 
 
-class ConfigCommand(RESTCommand):
+class ConfigCommand(WebhookdCommand):
 
     resource = 'config'
     _headers = {'Accept': 'application/json'}
