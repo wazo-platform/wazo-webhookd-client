d32424085faf249c5eb071e958db0fcf9a8e8a88
Francois Blackburn <fblackburn@wazo.community>
2021-10-12 15:38:08 -0400

    use get_headers from RESTCommand

--- wazo_webhookd_client/commands/config.py
+++ wazo_webhookd_client/commands/config.py
@@ -2,24 +2,21 @@
 # Copyright 2017-2021 The Wazo Authors  (see the AUTHORS file)
 # SPDX-License-Identifier: GPL-3.0-or-later
 
-
 from wazo_webhookd_client.command import WebhookdCommand
 
 
 class ConfigCommand(WebhookdCommand):
 
     resource = 'config'
-    _ro_headers = {'Accept': 'application/json'}
-    _rw_headers = {'Accept': 'application/json', 'Content-Type': 'application/json'}
 
     def get(self):
-        r = self.session.get(self.base_url, headers=self._ro_headers)
+        headers = self._get_headers()
+        r = self.session.get(self.base_url, headers=headers)
         self.raise_from_response(r)
         return r.json()
 
     def patch(self, config_patch):
-        r = self.session.patch(
-            self.base_url, headers=self._rw_headers, json=config_patch
-        )
+        headers = self._get_headers()
+        r = self.session.patch(self.base_url, headers=headers, json=config_patch)
         self.raise_from_response(r)
         return r.json()
--- wazo_webhookd_client/commands/subscriptions.py
+++ wazo_webhookd_client/commands/subscriptions.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-# Copyright 2017-2019 The Wazo Authors  (see the AUTHORS file)
+# Copyright 2017-2021 The Wazo Authors  (see the AUTHORS file)
 # SPDX-License-Identifier: GPL-3.0-or-later
 
 from wazo_webhookd_client.command import WebhookdCommand
@@ -8,19 +8,17 @@ from wazo_webhookd_client.command import WebhookdCommand
 class SubscriptionsCommand(WebhookdCommand):
 
     resource = 'subscriptions'
-    _ro_headers = {'Accept': 'application/json'}
-    _rw_headers = {'Accept': 'application/json', 'Content-Type': 'application/json'}
 
     def create(self, subscription):
-        r = self.session.post(
-            self.base_url, json=subscription, headers=self._rw_headers
-        )
+        headers = self._get_headers()
+        r = self.session.post(self.base_url, json=subscription, headers=headers)
         self.raise_from_response(r)
         return r.json()
 
     def create_as_user(self, subscription):
+        headers = self._get_headers()
         url = self._client.url('users', 'me', self.resource)
-        r = self.session.post(url, json=subscription, headers=self._rw_headers)
+        r = self.session.post(url, json=subscription, headers=headers)
         self.raise_from_response(r)
         return r.json()
 
@@ -30,7 +28,8 @@ class SubscriptionsCommand(WebhookdCommand):
             params['search_metadata'] = self._metadata_params(search_metadata)
         if recurse:
             params['recurse'] = True
-        r = self.session.get(self.base_url, params=params, headers=self._ro_headers)
+        headers = self._get_headers()
+        r = self.session.get(self.base_url, params=params, headers=headers)
         self.raise_from_response(r)
         return r.json()
 
@@ -38,56 +37,56 @@ class SubscriptionsCommand(WebhookdCommand):
         params = {}
         if search_metadata:
             params['search_metadata'] = self._metadata_params(search_metadata)
+        headers = self._get_headers()
         url = self._client.url('users', 'me', self.resource)
-        r = self.session.get(url, headers=self._ro_headers)
+        r = self.session.get(url, headers=headers)
         self.raise_from_response(r)
         return r.json()
 
     def get(self, subscription_uuid):
-        r = self.session.get(
-            '{base}/{id}'.format(base=self.base_url, id=subscription_uuid),
-            headers=self._ro_headers,
-        )
+        headers = self._get_headers()
+        url = self._client.url('subscriptions', subscription_uuid)
+        r = self.session.get(url, headers=headers)
         self.raise_from_response(r)
         return r.json()
 
     def get_as_user(self, subscription_uuid):
+        headers = self._get_headers()
         url = self._client.url('users', 'me', self.resource, subscription_uuid)
-        r = self.session.get(url, headers=self._ro_headers)
+        r = self.session.get(url, headers=headers)
         self.raise_from_response(r)
         return r.json()
 
     def update(self, subscription_uuid, subscription):
-        r = self.session.put(
-            '{base}/{id}'.format(base=self.base_url, id=subscription_uuid),
-            json=subscription,
-            headers=self._rw_headers,
-        )
+        headers = self._get_headers()
+        url = self._client.url('subscriptions', subscription_uuid)
+        r = self.session.put(url, json=subscription, headers=headers)
         self.raise_from_response(r)
         return r.json()
 
     def update_as_user(self, subscription_uuid, subscription):
+        headers = self._get_headers()
         url = self._client.url('users', 'me', self.resource, subscription_uuid)
-        r = self.session.put(url, json=subscription, headers=self._rw_headers)
+        r = self.session.put(url, json=subscription, headers=headers)
         self.raise_from_response(r)
         return r.json()
 
     def delete(self, subscription_uuid):
-        r = self.session.delete(
-            '{base}/{id}'.format(base=self.base_url, id=subscription_uuid),
-            headers=self._ro_headers,
-        )
+        headers = self._get_headers()
+        url = self._client.url('subscriptions', subscription_uuid)
+        r = self.session.delete(url, headers=headers)
         self.raise_from_response(r)
 
     def delete_as_user(self, subscription_uuid):
+        headers = self._get_headers()
         url = self._client.url('users', 'me', self.resource, subscription_uuid)
-        r = self.session.delete(url, headers=self._ro_headers)
+        r = self.session.delete(url, headers=headers)
         self.raise_from_response(r)
 
     def list_services(self):
-        r = self.session.get(
-            '{base}/services'.format(base=self.base_url), headers=self._ro_headers
-        )
+        headers = self._get_headers()
+        url = self._client.url('subscriptions', 'services')
+        r = self.session.get(url, headers=headers)
         self.raise_from_response(r)
         return r.json()
 
@@ -114,7 +113,8 @@ class SubscriptionsCommand(WebhookdCommand):
             params['offset'] = offset
         if from_date is not None:
             params['from_date'] = from_date
+        headers = self._get_headers()
         url = self._client.url(self.resource, subscription_uuid, 'logs')
-        r = self.session.get(url, headers=self._ro_headers, params=params)
+        r = self.session.get(url, headers=headers, params=params)
         self.raise_from_response(r)
         return r.json()
