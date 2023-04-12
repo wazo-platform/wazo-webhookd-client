89b93a6bd8b5edd11c207b309a1a5fcb03f2c749
Jesse Sopel <jesse.sopel@wazo.io>
2023-04-12 11:18:02 -0400

    Run black, since it has been updated

--- wazo_webhookd_client/commands/status.py
+++ wazo_webhookd_client/commands/status.py
@@ -1,11 +1,10 @@
-# Copyright 2017-2021 The Wazo Authors  (see the AUTHORS file)
+# Copyright 2017-2023 The Wazo Authors  (see the AUTHORS file)
 # SPDX-License-Identifier: GPL-3.0-or-later
 
 from wazo_webhookd_client.command import WebhookdCommand
 
 
 class StatusCommand(WebhookdCommand):
-
     resource = 'status'
 
     def get(self):
--- wazo_webhookd_client/commands/subscriptions.py
+++ wazo_webhookd_client/commands/subscriptions.py
@@ -1,11 +1,10 @@
-# Copyright 2017-2022 The Wazo Authors  (see the AUTHORS file)
+# Copyright 2017-2023 The Wazo Authors  (see the AUTHORS file)
 # SPDX-License-Identifier: GPL-3.0-or-later
 
 from wazo_webhookd_client.command import WebhookdCommand
 
 
 class SubscriptionsCommand(WebhookdCommand):
-
     resource = 'subscriptions'
 
     def create(self, subscription):
